e74f4c875e1409d4c5c8feba530b37d4addd463d
Andrés Víquez <73793020+avc01@users.noreply.github.com>
2022-05-30 21:49:00 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+const testUser = new User();
+const testCompany = new Company();
+console.log(testUser);
+console.log(testCompany);
